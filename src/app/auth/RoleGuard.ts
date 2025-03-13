3ce4492a5c53b0b0530ce45d1296ab181edf0c67
Abidkhan732 <abid.khan@averlabz.com>
2025-03-14 00:29:18 +0500

    icons change and role guad change

--- src/app/auth/RoleGuard.ts
+++ src/app/auth/RoleGuard.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
-  user = localStorage.getItem('roles');
-  constructor(public router: Router) {}
+  constructor(public router: Router,private authService: AuthService) {}
 
-  canActivate(route: ActivatedRouteSnapshot):  Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const requiredRoles = route.data['allowedRoles'] as string[]; 
-    if (this.user && requiredRoles.includes(this.user)) {
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const requiredRoles = route.data['allowedRoles'] as string[];
+    const currentUser = this.authService.getCurrentUser();
+  
+    const userRoles = currentUser?.roles || [];
+  
+    const hasAccess = userRoles.some((role: string) => requiredRoles.includes(role));
+  
+    if (hasAccess) {
       return true;
     } else {
       this.router.navigate(['/authentication/error']);
       return false;
     }
   }
+  
 }
