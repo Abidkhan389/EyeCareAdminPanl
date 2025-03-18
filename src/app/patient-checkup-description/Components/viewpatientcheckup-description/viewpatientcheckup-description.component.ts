c77df8a6067cd5fc93c706e31c24d1666eeca5e0
Abidkhan732 <abid.khan@averlabz.com>
2025-03-18 12:27:44 +0500

    patient view with go

--- src/app/patient-checkup-description/Components/viewpatientcheckup-description/viewpatientcheckup-description.component.ts
+++ src/app/patient-checkup-description/Components/viewpatientcheckup-description/viewpatientcheckup-description.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MaterialModule } from 'src/app/material.module';
+import { PatientAppointmentService } from 'src/app/patient-appointment/Services/patient-appointment.service';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PatientCheckUpDescriptionService } from '../../Services/patient-check-up-description.service';
+import { finalize } from 'rxjs';
+import { ResultMessages } from 'src/app/_common/constant';
+import { showErrorMessage } from 'src/app/_common/messages';
 
 @Component({
   selector: 'app-viewpatientcheckup-description',
@@ -11,5 +17,38 @@ import { SharedModule } from 'src/app/shared/shared.module';
   styleUrl: './viewpatientcheckup-description.component.scss'
 })
 export class ViewpatientcheckupDescriptionComponent {
+loading:boolean= true;
+  patientAppointmentCheckUpDescription:any
+  
+constructor(
+    private route: ActivatedRoute,
+    private patientCheckUpDescriptionService: PatientCheckUpDescriptionService,
+  ) {
+    
+  }
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const id = params['id']; // Get employee ID from the route
+      this.GetPatiencheckupDescription(id);
+    });
+  }
+  GetPatiencheckupDescription(id: any) {
+     this.loading = true;
+     let model = Object.assign({});
+     model.id = id
+     this.patientCheckUpDescriptionService.GetPatientDescriptionByIdForShowHistroy(model).pipe(
+       finalize(() => {
+         this.loading = false;
+       }))
+       .subscribe(result => {
+         if (result) {
+          this.patientAppointmentCheckUpDescription= result.data; 
+         }
+       },
+         error => {
+           showErrorMessage(ResultMessages.serverError);
+         });
+ 
+   }
 
 }
