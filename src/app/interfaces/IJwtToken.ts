4dd1be954c93df1054c39f83d29a717d1e2cb6f9
Abidkhan732 <abid.khan@averlabz.com>
2025-05-03 13:09:45 +0500

    toekn helper gets correctly now roles of the user

--- src/app/interfaces/IJwtToken.ts
+++ src/app/interfaces/IJwtToken.ts
@@ -3,7 +3,7 @@ export interface IJwtToken {
     exp: number,
     'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/email': string,
     'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name': string[],
-    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/role'?: string[] | string;
+    'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'?: string | string[];
     'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameIdentifier'?: string;  
     FullName:string,
     TimeZoneId:string;
